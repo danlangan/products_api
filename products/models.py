91a4b5989e307b79db3e2323843825cf1c3d9557
Dan Langan <danlangan@Dans-MBP-2.attlocal.net>
2022-07-07 14:57:47 -0500

    Created the class Product in the models.py folder and edited the settings to add the rest_framework and the products app into the installed apps section. Then migrated everything over to MySQL so that the table is created in MySQL Workbench

--- products/models.py
+++ products/models.py
@@ -1,3 +1,11 @@
+from cgi import print_exception
+from unicodedata import decimal
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory_quantity = models.IntegerField()
