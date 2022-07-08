83467091d5b0ac8ff4e392b92cd65e032deab7b6
Dan Langan <danlangan@Dans-MBP-2.attlocal.net>
2022-07-08 15:50:18 -0500

    added the delete function using the primary key indicator

--- products/views.py
+++ products/views.py
@@ -1,4 +1,3 @@
-import re
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -21,7 +20,7 @@ def products_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def products_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
@@ -32,3 +31,6 @@ def products_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
